8a29f399d250f789b7cfd02c7aae842434a8a4a6
wldnd9904 <wldnd9904@uos.ac.kr>
2024-01-31 10:17:36 +0900

    unary, once(thisValue binding)

--- js/24_unary.js
+++ js/24_unary.js
@@ -12,3 +12,13 @@ const unary = (fn) => (arg) => fn(arg);
 
 const rets2 = arr.map(unary(parseInt));
 console.log(rets2); // [ 1, 2, 3 ]
+Array.prototype.unaryMap = function (f) {
+  const result = [];
+  for (let i = 0; i < this.length; i += 1) {
+    result.push(f(this[i][0]));
+  }
+  return result;
+};
+
+const rets3 = arr.unaryMap(parseInt);
+console.log(rets3); // [ 1, 2, 3 ]
--- js/25_once.js
+++ js/25_once.js
@@ -1,15 +1,33 @@
-const once = (fn) => {
-  let done = false;
-  return (...args) => {
-    if (!done) {
-      done = true;
-      return fn(...args);
-    }
+function once(fn) {
+  let didRun = false;
+  return function (...args) {
+    if (!didRun) {
+      didRun = true;
+      return fn.call(this, ...args);
+    } else return;
   };
-};
+}
+const thisObj1 = { id: 201 };
+const thisObj2 = { id: 202 };
+function f(x, y) {
+  return `금일 운행금지 차량은 끝번호 ${x}, ${y}입니다! - ${this.id}`;
+}
+const fn = once(f);
 
-const fn = once((x, y) => `금일 운행금지 차량은 끝번호 ${x}, ${y}입니다!`);
-
-console.log(fn(1, 6)); // 금일 운행금지 차량은 끝번호 1, 6입니다!
-console.log(fn(2, 7)); // undefined
+console.log(fn.call(thisObj1, 1, 6)); // 금일 운행금지 차량은 끝번호 1, 6입니다!
+console.log(fn.call(thisObj2, 2, 7)); // undefined
 console.log(fn(3, 8)); // undefined
+
+// obj = {
+//   id: 1,
+//   incrementId() {
+//     this.id++;
+//     return this;
+//   },
+//   print() {
+//     console.log(this.id);
+//     return this;
+//   },
+// };
+
+// obj.incrementId().print().print().incrementId().print();
