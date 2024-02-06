abc5b128ddbc9009d0dfe9a2a5dd941222572282
wldnd9904 <wldnd9904@uos.ac.kr>
2024-02-06 12:52:09 +0900

    await fetch

--- js/41_quiz.js
+++ js/41_quiz.js
@@ -24,9 +24,16 @@ const getPosts = async (userID) => {
     comments: commentLists[idx].value,
   }));
 };
-console.log([...(await getPosts(1))]);
+
+//비동기 출력
+const getPosts2 = async (userID) => {
+  const posts = await fetch(postListURL(userID)).then((data) => data.json());
+  for (let i in posts) {
+    console.log({ ...posts[i], comments: await getComments(posts[i].id) });
+  }
+};
 
 //  1번 유저의 게시글 목록과 댓글을 리턴하는 함수를 작성하시오.
 // - 1번 유저의 글목록: https://jsonplaceholder.typicode.com/posts?userId=1
 // - 댓글 목록: https://jsonplaceholder.typicode.com/posts/<postId>/comments
-getPosts(1);
+getPosts2(1);
