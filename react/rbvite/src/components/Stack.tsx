5cc16e46b50a21cd1f0de992beb7e75dc5c08561
wldnd9904 <wldnd9904@uos.ac.kr>
2024-02-28 13:14:28 +0900

    stacks (non-styled-components)

--- react/rbvite/src/components/Stack.tsx
+++ react/rbvite/src/components/Stack.tsx
@@ -1,11 +1,19 @@
-import { styled } from "styled-components";
+import { HTMLAttributes, PropsWithChildren } from "react";
 
-export const VStack = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-export const HStack = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
+interface StackProps extends HTMLAttributes<HTMLDivElement> {
+  rotated?: boolean;
+}
+
+export const HStack = (props: PropsWithChildren<StackProps>) => {
+  const { children, className, rotated, ...attributes } = props;
+  return (
+    <div
+      {...attributes}
+      className={`flex flex-${rotated ? "col" : "row"} items-center gap-1 ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
+export const VStack = (props: PropsWithChildren<StackProps>) =>
+  HStack({ ...props, rotated: !props.rotated });
