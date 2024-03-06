fa9424f6971981409996a932795eea4477aad6bc
wldnd9904 <wldnd9904@uos.ac.kr>
2024-03-06 16:43:17 +0900

    middleware

---
+++ next/my-app/middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  console.log('&&&>>', request.cookies);
+  const didLogin = request.cookies.has('nextjs');
+  if (!didLogin) return NextResponse.redirect(new URL('/photos', request.url));
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ['/aaa', '/bbb/:path*'],
+};
